98f36da10e408ebd49e4e788753cea87c085768b
Art Goldhammer <art.goldhammer@gmail.com>
2021-02-03 08:25:28 -0500

    trouble enabling/disabling submit btn

--- src/lexy/input.cljs
+++ src/lexy/input.cljs
@@ -9,15 +9,26 @@
     #_(print "value" (. elt -value))
     (set! (. elt -value) nil)))
 
+(defn- enable-btn-with-id
+  "set enabled prop of btn with given id"
+  [id enable?]
+  (print "enable-btn-with-id called" id enable?)
+  (let [elt (. js/document getElementById id)]
+    #_(set! (. elt -disabled) (not t-or-f))
+    (if (not enable?)
+      (.setAttribute elt "disabled" true)
+      (.removeAttribute elt "disabled"))))
+
 (defn- clear-all-inputs
   "clear all edit input buttons"
   []
-  (mapv clear-btn-with-id ["edsrc" "edtgt" "edsupp"]))
+  (mapv clear-btn-with-id ["edsrc" "edtgt" "edsupp"])
+  (enable-btn-with-id "edsubmit" false))
 
-(defn clear-button
+(defn- clear-button
   "clear all edit boxes"
   []
-  [:button.button.is-rounded.is-danger.ml-4
+  [:button#eclear.button.is-rounded.is-danger.ml-4
    {:on-click clear-all-inputs}
    "Clear"])
 
@@ -30,9 +41,25 @@
 (defn- collect-values
   "collect values of all the input boxes"
   []
-  (let [vals
-        (mapv get-val-of-btn-with-id ["edsrc" "edtgt" "edsupp"])]
-    #_(print vals)
+  (mapv get-val-of-btn-with-id ["edsrc" "edtgt" "edsupp"]))
+
+(defn- non-blank?
+  "Is string blank?"
+  [s]
+  (not= s ""))
+
+(defn- ready-to-submit?
+  "are at least src and target non blank?"
+  []
+  (let [[src tgt _] (collect-values)]
+       (and (non-blank? src) (non-blank? tgt))))
+
+(defn- collect-values-and-submit
+  "collect and submit"
+  [e]
+  (print "cvas" e)
+  (let [vals (collect-values)]
+    (print "collect vals and submit called" vals)
     ;; TODO write submit function!!!
     (client/submit-new-slug vals)
     (clear-all-inputs)))
@@ -40,11 +67,18 @@
 (defn- submit-button
   "submit the new slug"
   []
-  [:button.button.is-rounded.is-success.ml-4
-   {:on-click collect-values
+  [:button#edsubmit.button.is-rounded.is-success.ml-4
+   {:on-click #(collect-values-and-submit %)
     ;; TODO: this should start disabled, enable only when condx satisfied
-    :disabled false}
+    :disabled true}
    "Submit"])
+
+(defn- edit-box-change
+  "called when any edit box changes"
+  [e]
+  (print "edit box change" e)
+  (let [submit-btn-enable? (ready-to-submit?)]
+    (enable-btn-with-id "edsubmit" submit-btn-enable?)))
 
 (defn- edit-box
   ""
@@ -59,7 +93,7 @@
        #_#_:value ""
        :placeholder ph
        :type "text"
-       #_#_:on-change #(modify-slug id %)}]]))
+       :on-change #(edit-box-change %)}]]))
 
 (defn edit-panel
   "view function for edit-panel"
@@ -73,5 +107,8 @@
 
 (comment
   (clear-btn-with-id "edsrc")
-  (get-val-of-btn-with-id "edsrc"))
+  (get-val-of-btn-with-id "edsrc")
+  (ready-to-submit?)
+  (collect-values-and-submit nil)
+  (. (. js/document getElementById "edsubmit") -onclick))
 
